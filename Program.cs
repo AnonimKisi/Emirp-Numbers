124a3df45f88e4df03ef4bd096d830e1910385ef
AnonimKisi <67228486+AnonimKisi@users.noreply.github.com>
2021-04-13 11:51:44 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,25 +1,23 @@
-﻿//Made by AnonimKisi
-//Add the neccessary libraries
+//Made by AnonimKisi
 using System;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        //declare the number
         static int number = 13;
-//the functions needed to reverse the 'number'
+
         public static int Reverse(int reverse)
         {
-            int temp_number = number; //Don't touch the actual number
+            int temp_number = number;
             reverse = 0;
-            Goto_Point:
+            goto_point:
             if (temp_number > 0)
             {
                 int reverse_flag = temp_number % 10;
                 reverse = (reverse * 10) + reverse_flag;
                 temp_number = temp_number / 10;
-                goto Goto_Point;
+                goto goto_point;
             }
             return reverse;
         }
@@ -27,22 +25,21 @@ namespace ConsoleApp1
         static void Main()
         {
             Console.Title = "Made by AnonimKisi";
-//Declare the neccessary variables
+
               int  half = number / 2, i, flag = 0;
 
                 Console.WriteLine("-------------------------------");           
-                Console.WriteLine("I wanna list all emirp number between 13 and X.");
+                Console.WriteLine("I want to list all emirp number between 13 and X.");
                 Console.WriteLine("X can be min. 100 and max. 1000\n");
                 Console.Write("X: ");           
 
                 int X = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine();
-//if 'X' is smaller than 100 or bigger than 1000, return to main()
+
                 if (X < 100 || X > 1000)
                 {
                     Main();
                 }
-            //if not...
                 else
                 {
                     while (number < X)
@@ -61,7 +58,7 @@ namespace ConsoleApp1
                         //if it is, start checking if its reverse also is
                         if (flag == 0)
                         {
-                            int reverse = Reverse(4);//choosed a number which isn't prime, on purpose
+                            int reverse = Reverse(6);//choosed a number which isn't prime, on purpose (avoided using 4 in respects to Mista) 
                             half = reverse / 2;
 
                             for (i = 2; i <= half; i++)
